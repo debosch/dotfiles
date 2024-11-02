238bf4b1aa1bbaa9c5417b52c53284ae4a0ef868
debosch <vlad.shiltsin@gmail.com>
2024-11-02 13:14:18 +0200

    Remove wezterm tab, if there is only one

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,11 +17,14 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.color_scheme = "tokyonight_night"
 config.font_size = 18
+
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
+
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
