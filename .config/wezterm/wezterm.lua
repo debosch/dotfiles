efa61746301b4aa67b1985f3150e132cc75f9dfc
debosch <vlad.shiltsin@gmail.com>
2024-11-08 12:22:59 +0200

    Increase font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("Hack Nerd Font Mono", { weight = "Bold" })
 config.color_scheme = "rose-pine-moon"
-config.font_size = 18
+config.font_size = 20
 
 config.window_padding = {
 	left = 0,
